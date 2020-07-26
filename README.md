8f609e06779065a20b9dd3a2fedc1c1d151097a8
Jack Yang <h36yang@gmail.com>
2020-07-26 03:37:28 -0400

    Finished WaitGroups and Mutexes

--- README.md
+++ README.md
@@ -11,3 +11,9 @@ Practice materials for "Concurrent Programming with Go" course
 |Have own execution stack|Have own execution stack|
 |Fixed stack space (~1MB)|Variable stack space (starts @2KB)|
 |Managed by OS|Managed by Go runtime|
+
+### Challenges with Concurrency
+* Coordinating tasks - `WaitGroups`
+  * A `WaitGroup` waits for a collection of goroutines to finish.
+* Shared memory - `Mutexes`
+  * A `mutex` is a **mut**ual **ex**clusion lock.
--- app/main.go
+++ app/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"math/rand"
+	"sync"
 	"time"
 )
 
@@ -10,38 +11,62 @@ var cache = map[int]Book{}
 var rnd = rand.New(rand.NewSource(time.Now().UnixNano()))
 
 func main() {
+	// Instantiate a pointer to a WaitGroup
+	wg := &sync.WaitGroup{}
+	// Instantiate a pointer to a Read/Write Mutex
+	mx := &sync.RWMutex{}
+
 	for i := 0; i < 10; i++ {
 		id := rnd.Intn(10) + 1
 
-		go func(id int) {
-			if b, ok := queryCache(id); ok {
+		// Add 2 concurrent processes to the WaitGroup
+		wg.Add(2)
+
+		go func(id int, wg *sync.WaitGroup, mx *sync.RWMutex) {
+			if b, ok := queryCache(id, mx); ok {
 				fmt.Println("from cache")
 				fmt.Println(b)
 			}
-		}(id)
+			// Mark current Goroutine in the WaitGroup as done
+			wg.Done()
+		}(id, wg, mx)
 
-		go func(id int) {
-			if b, ok := queryDatabase(id); ok {
+		go func(id int, wg *sync.WaitGroup, mx *sync.RWMutex) {
+			if b, ok := queryDatabase(id, mx); ok {
 				fmt.Println("from database")
 				fmt.Println(b)
 			}
-		}(id)
+			// Mark current Goroutine in the WaitGroup as done
+			wg.Done()
+		}(id, wg, mx)
 
 		time.Sleep(150 * time.Millisecond)
 	}
-	time.Sleep(2 * time.Second)
+
+	// Wait for all the processes in the WaitGroup to finish
+	wg.Wait()
 }
 
-func queryCache(id int) (Book, bool) {
+func queryCache(id int, mx *sync.RWMutex) (Book, bool) {
+	// Lock the resource for Read
+	// NOTE: Multiple Readers can access the same resource concurrently
+	mx.RLock()
 	b, ok := cache[id]
+	// Unlock the resource for Read
+	mx.RUnlock()
 	return b, ok
 }
 
-func queryDatabase(id int) (Book, bool) {
+func queryDatabase(id int, mx *sync.RWMutex) (Book, bool) {
 	time.Sleep(100 * time.Millisecond)
 	for _, b := range books {
 		if b.ID == id {
+			// Lock the resource for Write
+			// NOTE: Only a single Writer can access the resource at one time
+			mx.Lock()
 			cache[id] = b
+			// Unlock the resource for Write
+			mx.Unlock()
 			return b, true
 		}
 	}
